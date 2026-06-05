6773e4cac4d0f7ea5dbc6e4a749abc8cf3f5a594
marfushhaaa <p1m455w96@gmail.com>
2026-06-05 11:19:01 +0200

    feat: add all files for auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,62 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi, withXsrfConfiguration } from '@angular/common/http';
+import { ApplicationConfig, enableProdMode, importProvidersFrom, inject, provideBrowserGlobalErrorListeners, provideEnvironmentInitializer, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AuthConfig, OAuthStorage, provideOAuthClient } from 'angular-oauth2-oidc';
+import { HttpXSRFInterceptor } from './interceptor/http.csrf.interceptor';
+import { AppAuthService } from './service/app.auth.service';
+import { environment } from '../environments/environment';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+export const authConfig: AuthConfig = {
+  issuer: 'http://localhost:8080/realms/learnitself',
+  requireHttps: false,
+  redirectUri: environment.frontendBaseUrl,
+  postLogoutRedirectUri: environment.frontendBaseUrl,
+  clientId: 'learnitself',
+  scope: 'openid profile roles offline_access',
+  responseType: 'code',
+  showDebugInformation: true,
+  requestAccessToken: true,
+  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+  silentRefreshTimeout: 500,
+  clearHashAfterLogin: true,
+  waitForTokenInMsec: 1000
+};
+
+export function storageFactory(): OAuthStorage {
+  return sessionStorage;
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
+    importProvidersFrom(
+      BrowserModule
+    ),
+    { provide: AuthConfig, useValue: authConfig },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpXSRFInterceptor, multi: true },
+    {
+      provide: OAuthStorage,
+      useFactory: storageFactory,
+    },
+    Location,
+    { provide: LocationStrategy, useClass: PathLocationStrategy },
+    provideHttpClient(
+      withInterceptorsFromDi(),
+      withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN',
+        headerName: 'X-XSRF-TOKEN',
+      })
+    ),
+    provideOAuthClient({ resourceServer: { sendAccessToken: true, allowedUrls: [environment.backendBaseUrl], } }),
+    provideEnvironmentInitializer(() => { inject(AppAuthService).initAuth().finally() }),
     provideRouter(routes)
   ]
 };
