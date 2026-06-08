80730923c941d7e2e809d282a41f890486484df4
marfushhaaa <p1m455w96@gmail.com>
2026-06-08 09:34:11 +0200

    feat: add new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {NoAccessComponent} from './pages/no-access/no-access.component';
 import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {CoursesComponent} from './pages/courses/courses.component';
 
 export const routes: Routes = [
    {path: '', component: DashboardComponent},
    {path: 'noaccess', component: NoAccessComponent},
+   {path: 'courses', component: CoursesComponent},
 ];
---
+++ src/app/pages/courses/courses.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  imports: [],
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.css',
+})
+export class CoursesComponent {}
